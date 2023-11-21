06bf55cbda9f5ec59418848c585b0168e03aade3
Sergey Smirnov <89378719+sergey-grovety@users.noreply.github.com>
2023-11-21 16:56:14 +0300

    new SDK added

--- src/GRC_AI.cpp
+++ src/GRC_AI.cpp
@@ -1,21 +1,39 @@
-#include <GRC.h>
+#include "GRC_AI.h"
+#include "grc/i2c/drivers/arduino/grc_ll_i2c_arduino_impl.h"
 
 
-GRC::GRC(Wire& wire, uint8_t slaveAddress)
+GRC_AI::GRC_AI(uint32_t arch, TwoWire& wire)
 {
+    // ll_dev.type = PROTOCOL_INTERFACE_I2C_ARDUINO;
+    // ll_dev.arduino_wire = &wire;
+    // dev.ll_dev = &ll_dev;
 
+    // grc_config cfg = {
+    //     .arch = arch,
+    // };
+
+    // int res = grc_init(&dev, &cfg);
+    // printf("init failed with code: %d\n", res);
+    // if (res < 0)
+    //     throw res;
 }
 
-GRC::~GRC()
+GRC_AI::~GRC_AI()
 {
-
+    // grc_release(&dev);
 }
 
-int GRC::begin()
+uint32_t GRC_AI::version() const
 {
+    return dev.version;
 }
 
-void GRC::end()
+int GRC_AI::begin()
+{
+    return 0;
+}
+
+void GRC_AI::end()
 {
 
 }
--- src/GRC_AI.h
+++ src/GRC_AI.h
@@ -1,18 +1,26 @@
-#ifndef _GRC_H_
-#define _GRC_H_
+#ifndef _GRC_AI_H_
+#define _GRC_AI_H_
 
 #include <Arduino.h>
 #include <Wire.h>
 
-class GRC {
+#include "grc/grc.h"
+#include "grc/i2c/drivers/arduino/grc_ll_i2c_arduino.h"
+
+class GRC_AI {
 public:
-    GRC(TwoWire& wire, uint8_t slaveAddress);
-    ~GRC();
+    GRC_AI(uint32_t arch, TwoWire& wire);
+    ~GRC_AI();
+
+    uint32_t version() const;
 
     int begin();
     void end();
 
+private:
+    grc_device dev;
 
-}
 
-#endif // _GRC_H_
+};
+
+#endif // _GRC_AI_H_
